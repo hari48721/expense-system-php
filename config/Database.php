bb5c1acce86bf903a2d23e963616239138a85ea7
unknown <hari48721@gmail.com>
2023-02-05 14:55:37 +0530

    daabase

--- config/Database.php
+++ config/Database.php
@@ -3,9 +3,9 @@ session_start();
 class Database{
 	
 	private $host  = 'localhost';
-    private $user  = 'root';
-    private $password   = "";
-    private $database  = "webdamn_demo"; 
+    private $user  = 'u587644132_expuser';
+    private $password   = "public@123";
+    private $database  = "u587644132_expsystemmvc"; 
     
     public function getConnection(){		
 		$conn = new mysqli($this->host, $this->user, $this->password, $this->database);
